52e6d111d987519a6f16bd363747b2f0d4bc430f
Your Name <you@example.com>
2022-11-15 12:57:50 +0100

    Identified erroneous run for MDSA.

--- tests/sparse/MDSA/test_snn_results.py
+++ tests/sparse/MDSA/test_snn_results.py
@@ -6,7 +6,7 @@ import shutil
 import unittest
 
 from snncompare.exp_setts.custom_setts.run_configs.algo_test import (
-    experiment_config_for_mdsa_testing,
+    long_exp_setts_for_mdsa_testing,
 )
 from snncompare.exp_setts.Supported_experiment_settings import (
     Supported_experiment_settings,
@@ -31,7 +31,7 @@ class Test_mdsa_snn_results(unittest.TestCase):
     def __init__(self, *args, **kwargs):  # type: ignore[no-untyped-def]
         super().__init__(*args, **kwargs)
         self.algorithms = {
-            "MDSA": get_algo_configs(MDSA(list(range(0, 2, 1))).__dict__)
+            "MDSA": get_algo_configs(MDSA(list(range(0, 7, 1))).__dict__)
         }
 
     @typechecked
@@ -47,9 +47,7 @@ class Test_mdsa_snn_results(unittest.TestCase):
             shutil.rmtree("latex")
 
         # Generate default experiment config.
-        mdsa_creation_only_size_3_4: dict = (
-            experiment_config_for_mdsa_testing()
-        )
+        mdsa_creation_only_size_3_4: dict = long_exp_setts_for_mdsa_testing()
 
         # Do not output images.
         mdsa_creation_only_size_3_4["overwrite_visualisation"] = True
