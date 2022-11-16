89c9aeeba28f15f680f2bc3e98e38ec601e3eaf9
Your Name <you@example.com>
2022-11-16 21:24:55 +0100

    Included explicit verification from json results in test.

--- tests/sparse/MDSA/test_snn_results.py
+++ tests/sparse/MDSA/test_snn_results.py
@@ -1,11 +1,11 @@
 """Tests whether the snn MDSA algorithm results equal those of the
 default/Neumann implementation."""
 # pylint: disable=R0801
+import copy
 import os
 import shutil
 import unittest
-from pprint import pprint
-from typing import Any
+from typing import Any, Dict
 
 from snncompare.exp_setts.custom_setts.run_configs.algo_test import (
     minimal_mdsa_test_exp_setts,
@@ -77,9 +77,9 @@ class Test_mdsa_snn_results(unittest.TestCase):
         # Experiment_runner(
         # mdsa_creation_only_size_3_4, run_config_with_error())
         exp_runner = Experiment_runner(mdsa_creation_only_size_3_4)
-        assert_run_config_json_results(
-            self, exp_runner, exp_runner.run_configs[0]
-        )
+
+        for run_config in exp_runner.run_configs:
+            assert_run_config_json_results(self, exp_runner, run_config)
 
 
 @typechecked
@@ -88,22 +88,46 @@ def assert_run_config_json_results(
 ) -> None:
     """Verifies the results of a run config using the json result output."""
 
-    json_graphs = load_json_to_nx_graph_from_file(
+    nx_graphs = load_json_to_nx_graph_from_file(
         run_config=run_config, stage_index=4, to_run=exp_runner.to_run
     )
-    pprint(json_graphs["input_graph"])
-
-    # TODO: convert json graphs to nx graphs.
 
     # Verify results are as expected.
-    alipour_counter_marks = get_results(
-        input_graph=json_graphs["input_graph"],
+    expected_nodenames: Dict[str, int] = get_results(
+        input_graph=nx_graphs["input_graph"],
         iteration=run_config["iteration"],
         m_val=run_config["algorithm"]["MDSA"]["m_val"],
-        rand_props=json_graphs["input_graph"].graph["alg_props"],
+        rand_props=nx_graphs["input_graph"].graph["alg_props"],
         seed=run_config["seed"],
         size=run_config["graph_size"],
     )
 
-    alipour_counter_marks = alipour_counter_marks + 1
-    test_object.assertEquals(1, 2 - 1)
+    for graph_name, snn_graph in nx_graphs.items():
+
+        if graph_name in ["snn_algo_graph", "adapted_snn_graph"]:
+
+            # Verify the SNN graphs have completed simulation stage 2.
+            test_object.assertTrue(
+                # pylint: disable=R1733
+                2
+                in nx_graphs[graph_name].graph["completed_stages"]
+            )
+
+            actual_nodenames: Dict[str, int] = snn_graph.graph["results"]
+
+            # Remove the passed boolean, and redo results verification.
+            copy_actual_nodenames = copy.deepcopy(actual_nodenames)
+            copy_actual_nodenames.pop("passed")
+
+            # Verify node names are identical.
+            test_object.assertEquals(
+                copy_actual_nodenames.keys(), expected_nodenames.keys()
+            )
+
+            # Verify the expected nodes are the same as the actual nodes.
+            for key, expected_val in expected_nodenames.items():
+                test_object.assertEquals(
+                    expected_val, copy_actual_nodenames[key]
+                )
+
+            test_object.assertTrue(actual_nodenames["passed"])
