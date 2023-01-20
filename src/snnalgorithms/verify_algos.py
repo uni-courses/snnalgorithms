42e3f1b8b6dfa0576f07869d47056ad7b01589fe
a-t-0 <you@example.com>
2023-01-20 15:03:49 +0100

    Before moving supported settings into exp_config.

--- src/snnalgorithms/verify_algos.py
+++ src/snnalgorithms/verify_algos.py
@@ -1,16 +1,18 @@
 """Used to verify the algorithm specifications in an experiment
 configuration."""
-from typing import List, Union
+from typing import TYPE_CHECKING, List, Union
 
-from snncompare.exp_config.Exp_config import Exp_config
 from typeguard import typechecked
 
 from .get_alg_configs import verify_algo_configs
 from .helper import assert_parameter_is_list
 
+if TYPE_CHECKING:
+    from snncompare.exp_config.Exp_config import Exp_config
+
 
 @typechecked
-def verify_algos_in_exp_config(exp_config: Exp_config) -> None:
+def verify_algos_in_exp_config(exp_config: "Exp_config") -> None:
     """Verifies an algorithm specification is valid."""
     for algo_name, algo_spec in exp_config.algorithms.items():
         if algo_name == "MDSA":
