94acb5b2f22b43884a9e6372bd88e6e82c5ade38
name <example@example.com>
2023-02-28 15:51:14 +0100

    Included neuron discovery module.

--- src/snnalgorithms/sparse/Discovery/Discovery.py
+++ src/snnalgorithms/sparse/Discovery/Discovery.py
@@ -3,7 +3,7 @@ settings."""
 import sys
 
 import networkx as nx
-from snnbackends.networkx.LIF_neuron import LIF_neuron
+from snnbackends.networkx.LIF_neuron import LIF_neuron, Synapse
 from snnbackends.networkx.run_on_networkx import (
     create_neuron_for_next_timestep,
     run_simulation_with_networkx_for_1_timestep,
@@ -30,32 +30,36 @@ class Discovery:
         # Specify supported values for u
         self.u_min: float = -10
         self.u_max: float = 10
-        self.u_len: int = 40
+        self.u_len: int = 20
 
         # Specify supported values for du.
         self.du_min: float = -2
         self.du_max: float = 2
-        self.du_len: int = 40
+        self.du_len: int = 20
 
         # Specify svpported valves for v
         self.v_min: float = -10
         self.v_max: float = 10
-        self.v_len: int = 40
+        self.v_len: int = 20
 
         # Specify svpported valves for dv.
         self.dv_min: float = -2
         self.dv_max: float = 2
-        self.dv_len: int = 40
+        self.dv_len: int = 20
 
         # Specify supported biasalbiases for bias
         self.bias_min: float = -10
         self.bias_max: float = 10
-        self.bias_len: int = 40
+        self.bias_len: int = 20
 
         # Specify supported vthalvthes for vth
         self.vth_min: float = -10
-        self.vth_max: float = 20
-        self.vth_len: int = 40
+        self.vth_max: float = 10
+        self.vth_len: int = 20
+
+        # Specify supported vthalvthes for vth
+        self.weight_min: int = -10
+        self.weight_max: int = 10
 
 
 # pylint: disable=R0903
@@ -65,7 +69,7 @@ class Discovery_algo:
 
     @typechecked
     def __init__(self, disco: Discovery) -> None:
-        max_time: int = 10
+        max_time: int = 10000
         for du in [
             disco.du_min + i * (disco.du_max - disco.du_min) / disco.du_len
             for i in range(disco.du_len)
@@ -86,24 +90,29 @@ class Discovery_algo:
                         / disco.bias_len
                         for i in range(disco.bias_len)
                     ]:
-                        # Create neuron.
-                        lif_neuron = LIF_neuron(
-                            name="",
-                            bias=bias,
-                            du=du,
-                            dv=dv,
-                            vth=vth,
-                        )
+                        for weight in range(
+                            disco.weight_min, disco.weight_max
+                        ):
+                            # Create neuron.
+                            lif_neuron = LIF_neuron(
+                                name="",
+                                bias=bias,
+                                du=du,
+                                dv=dv,
+                                vth=vth,
+                            )
 
                         if self.is_expected_neuron_I(
-                            lif_neuron=lif_neuron, max_time=max_time
+                            lif_neuron=lif_neuron,
+                            max_time=max_time,
+                            weight=weight,
                         ):
                             print(f"du={du},dv={dv},vth={vth},bias={bias}")
                             print("FOUND")
                             sys.exit()
 
     def is_expected_neuron_I(
-        self, lif_neuron: LIF_neuron, max_time: int
+        self, lif_neuron: LIF_neuron, max_time: int, weight: int
     ) -> bool:
         """Determines whether a neuron is of type I.
 
@@ -113,10 +122,19 @@ class Discovery_algo:
         """
 
         snn_graph = nx.DiGraph()
+        node_name: str = "0"
         snn_graph.add_nodes_from(
-            [0],
+            [node_name],
         )
-        snn_graph.nodes[0]["nx_lif"] = [lif_neuron]
+        snn_graph.nodes[node_name]["nx_lif"] = [lif_neuron]
+        snn_graph.add_edges_from(
+            [(node_name, node_name)],
+            synapse=Synapse(
+                weight=weight,
+                delay=0,
+                change_per_t=0,
+            ),
+        )
 
         # Simulate neuron for at most max_time timesteps, as long as it behaves
         # as desired.
@@ -135,7 +153,7 @@ class Discovery_algo:
             # print(f't={t}, spikes={snn_graph.nodes[0]["nx_lif"][t].spikes:}')
 
             # If neuron behaves, continue, otherwise move on to next neuron.
-            if snn_graph.nodes[0]["nx_lif"][
+            if snn_graph.nodes[node_name]["nx_lif"][
                 t
             ].spikes != self.expected_spike_pattern_I(t):
                 return False
@@ -144,6 +162,6 @@ class Discovery_algo:
 
     def expected_spike_pattern_I(self, t: int) -> bool:
         """Specifies the expected spike pattern for neuron type I."""
-        if t < 2:
+        if t < 3:
             return False
         return True
