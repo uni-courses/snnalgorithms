0edaff33da655e1ca3d82534294a1bde1383d23e
name <example@example.com>
2023-02-28 15:43:16 +0100

    Included neuron discovery module.

--- src/snnalgorithms/__main__.py
+++ src/snnalgorithms/__main__.py
@@ -3,12 +3,13 @@ that invokes this script."""
 
 # Import code belonging to this project.
 
-from .get_alg_configs import get_algo_configs, verify_algo_configs
-from .sparse.MDSA.alg_params import MDSA
+from snnalgorithms.sparse.Discovery.Discovery import Discovery, Discovery_algo
 
-mdsa = MDSA(list(range(0, 4, 1)))
-mdsa_configs = get_algo_configs(algo_spec=mdsa.__dict__)
-verify_algo_configs(algo_name="MDSA", algo_configs=mdsa_configs)
+# mdsa = MDSA(list(range(0, 4, 1)))
+# mdsa_configs = get_algo_configs(algo_spec=mdsa.__dict__)
+# verify_algo_configs(algo_name="MDSA", algo_configs=mdsa_configs)
 
 # Parse command line interface arguments to determine what this script does.
 # args = parse_cli_args()
+disco = Discovery()
+Discovery_algo(disco=disco)
---
+++ src/snnalgorithms/sparse/Discovery/Discovery.py
@@ -0,0 +1,149 @@
+"""Contains the specification of and maximum values of the algorithm
+settings."""
+import sys
+
+import networkx as nx
+from snnbackends.networkx.LIF_neuron import LIF_neuron
+from snnbackends.networkx.run_on_networkx import (
+    create_neuron_for_next_timestep,
+    run_simulation_with_networkx_for_1_timestep,
+)
+from snnbackends.verify_graph_is_snn import verify_networkx_snn_spec
+from typeguard import typechecked
+
+
+# pylint: disable=R0902
+# pylint: disable=R0903
+class Discovery:
+    """Specification of algorithm specification. Algorithm: Minimum Dominating
+    Set Approximation by Alipour.
+
+    Example usage: default_MDSA_alg=MDSA(some_vals=list(range(0, 4, 1)))
+    """
+
+    @typechecked
+    def __init__(
+        self,
+    ) -> None:
+        self.name = "Discovery"
+
+        # Specify supported values for u
+        self.u_min: float = -10
+        self.u_max: float = 10
+        self.u_len: int = 40
+
+        # Specify supported values for du.
+        self.du_min: float = -2
+        self.du_max: float = 2
+        self.du_len: int = 40
+
+        # Specify svpported valves for v
+        self.v_min: float = -10
+        self.v_max: float = 10
+        self.v_len: int = 40
+
+        # Specify svpported valves for dv.
+        self.dv_min: float = -2
+        self.dv_max: float = 2
+        self.dv_len: int = 40
+
+        # Specify supported biasalbiases for bias
+        self.bias_min: float = -10
+        self.bias_max: float = 10
+        self.bias_len: int = 40
+
+        # Specify supported vthalvthes for vth
+        self.vth_min: float = -10
+        self.vth_max: float = 20
+        self.vth_len: int = 40
+
+
+# pylint: disable=R0903
+# pylint: disable=R0801
+class Discovery_algo:
+    """Create a particular configuration for the neuron Discovery algorithm."""
+
+    @typechecked
+    def __init__(self, disco: Discovery) -> None:
+        max_time: int = 10
+        for du in [
+            disco.du_min + i * (disco.du_max - disco.du_min) / disco.du_len
+            for i in range(disco.du_len)
+        ]:
+            for dv in [
+                disco.dv_min + i * (disco.dv_max - disco.dv_min) / disco.dv_len
+                for i in range(disco.dv_len)
+            ]:
+                for vth in [
+                    disco.vth_min
+                    + i * (disco.vth_max - disco.vth_min) / disco.vth_len
+                    for i in range(disco.vth_len)
+                ]:
+                    for bias in [
+                        disco.bias_min
+                        + i
+                        * (disco.bias_max - disco.bias_min)
+                        / disco.bias_len
+                        for i in range(disco.bias_len)
+                    ]:
+                        # Create neuron.
+                        lif_neuron = LIF_neuron(
+                            name="",
+                            bias=bias,
+                            du=du,
+                            dv=dv,
+                            vth=vth,
+                        )
+
+                        if self.is_expected_neuron_I(
+                            lif_neuron=lif_neuron, max_time=max_time
+                        ):
+                            print(f"du={du},dv={dv},vth={vth},bias={bias}")
+                            print("FOUND")
+                            sys.exit()
+
+    def is_expected_neuron_I(
+        self, lif_neuron: LIF_neuron, max_time: int
+    ) -> bool:
+        """Determines whether a neuron is of type I.
+
+        Type I is arbitrarily defined as: 'does not spike for 2
+        timesteps, and then spikes indefinitely.'. (Because I would like
+        to use such a neuron.).
+        """
+
+        snn_graph = nx.DiGraph()
+        snn_graph.add_nodes_from(
+            [0],
+        )
+        snn_graph.nodes[0]["nx_lif"] = [lif_neuron]
+
+        # Simulate neuron for at most max_time timesteps, as long as it behaves
+        # as desired.
+        for t in range(0, max_time):
+            # Copy the neurons into the new timestep.
+            verify_networkx_snn_spec(snn_graph=snn_graph, t=t, backend="nx")
+            create_neuron_for_next_timestep(snn_graph=snn_graph, t=t)
+
+            verify_networkx_snn_spec(
+                snn_graph=snn_graph, t=t + 1, backend="nx"
+            )
+            # Simulate neuron.
+            run_simulation_with_networkx_for_1_timestep(
+                snn_graph=snn_graph, t=t + 1
+            )
+            # print(f't={t}, spikes={snn_graph.nodes[0]["nx_lif"][t].spikes:}')
+
+            # If neuron behaves, continue, otherwise move on to next neuron.
+            if snn_graph.nodes[0]["nx_lif"][
+                t
+            ].spikes != self.expected_spike_pattern_I(t):
+                return False
+
+        return True
+
+    def expected_spike_pattern_I(self, t: int) -> bool:
+        """Specifies the expected spike pattern for neuron type I."""
+        if t < 2:
+            return False
+        return True
