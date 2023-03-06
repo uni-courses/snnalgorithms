9c7cb3f590dfb8f5a4815229e959ae2466123b61
name <example@example.com>
2023-03-06 21:36:50 +0100

    Refactored redundancy test.

--- tests/sparse/MDSA/adaptation/redundancy_helper.py
+++ tests/sparse/MDSA/adaptation/redundancy_helper.py
@@ -3,13 +3,15 @@
 Then asserts the redundant `n` neuron spikes `n` timesteps after the
 neuron would spike in the unradiated version.
 """
-
-
-from typing import Any, Dict, List, Union
+import copy
+from pprint import pprint
+from typing import Any, Dict, List, Tuple, Union
 
 import networkx as nx
 from snncompare.exp_config.Exp_config import Exp_config
+from snncompare.Experiment_runner import Experiment_runner
 from snncompare.export_plots.create_dash_plot import create_svg_plot
+from snncompare.export_plots.Plot_config import get_default_plot_config
 from snncompare.export_results.helper import run_config_to_filename
 from snncompare.optional_config.Output_config import (
     Extra_storing_config,
@@ -17,11 +19,13 @@ from snncompare.optional_config.Output_config import (
     Output_config,
     Zoom,
 )
-from snncompare.run_config import Run_config
+from snncompare.run_config.Run_config import Run_config
+from snnradiation.Radiation_damage import verify_radiation_is_applied
 from typeguard import typechecked
 
 from snnalgorithms.get_alg_configs import get_algo_configs
 from snnalgorithms.sparse.MDSA.alg_params import MDSA
+from snnalgorithms.sparse.MDSA.get_results import get_results
 
 
 @typechecked
@@ -74,7 +78,7 @@ def assert_redundant_neuron_takes_over(
 
 @typechecked
 def get_redundant_neuron_names(
-    max_redundancy: int, original_node_name: str
+    *, max_redundancy: int, original_node_name: str
 ) -> List[str]:
     """Returns the names of the redundant neurons of a node."""
     redundant_neuron_names: List[str] = []
@@ -126,6 +130,7 @@ def perform_verification_for_each_spike_time(
 
 @typechecked
 def adapted_neuron_has_taken_over(
+    *,
     dead_neuron_name: str,
     graphs_dict: Dict,
     max_redundancy: int,
@@ -170,7 +175,7 @@ def adapted_neuron_has_taken_over(
 
 
 @typechecked
-def create_default_hover_info(exp_config: Exp_config) -> Hover_info:
+def create_default_hover_info(*, exp_config: Exp_config) -> Hover_info:
     """Create duplicate Hover_info that is used to generate the data belonging
     to each run config, using the Experiment runner."""
 
@@ -196,7 +201,7 @@ def create_default_hover_info(exp_config: Exp_config) -> Hover_info:
 
 
 @typechecked
-def create_default_output_config(exp_config: Exp_config) -> Output_config:
+def create_default_output_config(*, exp_config: Exp_config) -> Output_config:
     """Create duplicate Output_config that is used to generate the data
     belonging to each run config, using the Experiment runner."""
     output_config = Output_config(
@@ -228,7 +233,7 @@ def long_exp_config_for_mdsa_testing_with_adaptation() -> Exp_config:
     # Create the experiment configuration settings for a run with adaptation
     # and with radiation.
     long_mdsa_testing: Dict = {
-        "adaptations": {"redundancy": [2, 4, 6]},
+        "adaptations": {"redundancy": [2, 4]},
         "algorithms": {
             "MDSA": get_algo_configs(
                 algo_spec=MDSA(list(range(0, 6, 1))).__dict__
@@ -255,3 +260,150 @@ def long_exp_config_for_mdsa_testing_with_adaptation() -> Exp_config:
     # The ** loads the dict into the object.
     exp_config = Exp_config(**long_mdsa_testing)
     return exp_config
+
+
+@typechecked
+def get_dead_neuron_name_cominations(
+    *,
+    snn_algo_graph: nx.DiGraph,
+) -> List[List[str]]:
+    """Returns dead neuron lists."""
+    combinations: List[List[str]] = []
+
+    for node_name in snn_algo_graph.nodes():
+        if "connector" not in node_name:
+            combinations.append([node_name])
+    return combinations
+
+
+@typechecked
+def assert_run_config_json_results(
+    *,
+    test_object: Any,
+    graphs_dict: Dict[str, Union[nx.Graph, nx.DiGraph]],
+    run_config: Run_config,
+) -> None:
+    """Verifies the results of a run config using the json result output.
+
+    TODO: update expected_stages.
+    """
+
+    # Verify results are as expected.
+    expected_node_names: Dict[str, int] = get_results(
+        input_graph=graphs_dict["input_graph"],
+        m_val=run_config.algorithm["MDSA"]["m_val"],
+        rand_props=graphs_dict["input_graph"].graph["alg_props"],
+        seed=run_config.seed,
+        size=run_config.graph_size,
+    )
+
+    for graph_name, snn_graph in graphs_dict.items():
+        if graph_name in ["snn_algo_graph", "adapted_snn_graph"]:
+            # Verify the SNN graphs have completed simulation stage 2.
+            test_object.assertTrue(
+                # pylint: disable=R1733
+                2
+                in graphs_dict[graph_name].graph["completed_stages"]
+            )
+
+            actual_node_names: Dict[str, int] = snn_graph.graph["results"]
+
+            # Remove the passed boolean, and redo results verification.
+            copy_actual_node_names = copy.deepcopy(actual_node_names)
+            copy_actual_node_names.pop("passed")
+
+            # Verify node names are identical.
+            test_object.assertEquals(
+                copy_actual_node_names.keys(), expected_node_names.keys()
+            )
+
+            # Verify the expected nodes are the same as the actual nodes.
+            for key, expected_val in expected_node_names.items():
+                test_object.assertEquals(
+                    expected_val, copy_actual_node_names[key]
+                )
+
+            test_object.assertTrue(actual_node_names["passed"])
+
+
+@typechecked
+def get_run_config_and_results_dicts_for_large_test_scope() -> (
+    Tuple[Dict[Run_config, Dict], Output_config]
+):
+    """Sets up the long mdsa test scope for MDSA redundancy testing."""
+    mdsa_settings: Exp_config = (
+        long_exp_config_for_mdsa_testing_with_adaptation()
+    )
+    print("")
+    print("mdsa_settings=")
+    pprint(mdsa_settings.__dict__)
+
+    # Generate test scope.
+    mdsa_settings.export_types = None
+    output_config: Output_config = create_default_output_config(
+        exp_config=mdsa_settings
+    )
+
+    # Generate run_configs
+    full_exp_runner = Experiment_runner(
+        exp_config=mdsa_settings,
+        output_config=output_config,
+        reverse=True,
+        perform_run=False,
+        specific_run_config=None,
+    )
+
+    run_config_results: Dict[Run_config, Dict] = {}
+    for run_config in full_exp_runner.run_configs:
+        # Only test run_configs with adaptation for this test.
+        if list(run_config.adaptation.keys()) == ["redundancy"]:
+            # Get the original results dict to manually execute experiment.
+            original_results_nx_graphs: Dict = (
+                full_exp_runner.perform_run_stage_1(
+                    exp_config=mdsa_settings,
+                    output_config=output_config,
+                    plot_config=get_default_plot_config(),
+                    run_config=run_config,
+                )
+            )
+            run_config_results[run_config] = original_results_nx_graphs
+    return run_config_results, output_config
+
+
+@typechecked
+def overwrite_radiation_with_custom(
+    *, original_results_nx_graphs: Dict, dead_neuron_names: List[str]
+) -> Dict:
+    """Copies the existing adapted graph without radiation, into the
+    rad_adapted_snn_graph, and then applies custom radiation to it."""
+
+    # Create a deep copy from the results dict to prevent
+    # the run_config from working with data changed by
+    # the/a previous run_config.
+    results_nx_graphs = copy.deepcopy(original_results_nx_graphs)
+
+    # Copy adapted graph into radiation graph to overwrite
+    # radiation death. This is because the run_config uses
+    # the seed to set radiation death, whereas this test
+    # requires a specific set (dead_neuron_names) to die.
+    results_nx_graphs["graphs_dict"]["rad_adapted_snn_graph"] = copy.deepcopy(
+        results_nx_graphs["graphs_dict"]["adapted_snn_graph"]
+    )
+    rad_adapted_snn_graph = results_nx_graphs["graphs_dict"][
+        "rad_adapted_snn_graph"
+    ]
+
+    # Apply radiation based on the selected
+    # dead_neuron_names for this run_config.
+    for dead_neuron_name in dead_neuron_names:
+        rad_adapted_snn_graph.nodes[dead_neuron_name]["rad_death"] = True
+        rad_adapted_snn_graph.nodes[dead_neuron_name]["nx_lif"][0].vth.set(
+            9999
+        )
+    verify_radiation_is_applied(
+        some_graph=rad_adapted_snn_graph,
+        dead_neuron_names=dead_neuron_names,
+        rad_type="neuron_death",
+    )
+
+    return results_nx_graphs
--- tests/sparse/MDSA/adaptation/test_redundancy_1.py
+++ tests/sparse/MDSA/adaptation/test_redundancy_1.py
@@ -1,29 +1,21 @@
 """Tests whether the `nth` redundant neuron (with n=1) in the MDSA algorithm
 takes over from the died neurons (0 to n-1)."""
 # pylint: disable=R0801
-import copy
-from pprint import pprint
-from typing import Any, Dict, List, Union
 
-import networkx as nx
-from snncompare.exp_config import Exp_config
-from snncompare.Experiment_runner import Experiment_runner
-from snncompare.export_plots.Plot_config import get_default_plot_config
-from snncompare.optional_config.Output_config import Output_config
+from pprint import pprint
+
 from snncompare.process_results.process_results import (
     compute_results,
     set_results,
 )
-from snncompare.run_config import Run_config
 from snncompare.simulation.stage2_sim import sim_graphs
-from snnradiation.Radiation_damage import verify_radiation_is_applied
 from typeguard import typechecked
 
-from snnalgorithms.sparse.MDSA.get_results import get_results
-from tests.sparse.MDSA.adaptation.helper import (
+from tests.sparse.MDSA.adaptation.redundancy_helper import (
     assert_redundant_neuron_takes_over,
-    create_default_output_config,
-    long_exp_config_for_mdsa_testing_with_adaptation,
+    get_dead_neuron_name_cominations,
+    get_run_config_and_results_dicts_for_large_test_scope,
+    overwrite_radiation_with_custom,
 )
 from tests.sparse.MDSA.test_snn_results import Test_mdsa_snn_results
 
@@ -43,186 +35,64 @@ class Test_mdsa(Test_mdsa_snn_results):
     def test_something(self) -> None:
         """Tests whether the MDSA algorithm with adaptation yields the same
         results as without adaptation."""
+        (
+            run_config_results,
+            output_config,
+        ) = get_run_config_and_results_dicts_for_large_test_scope()
 
-        mdsa_settings: Exp_config = (
-            long_exp_config_for_mdsa_testing_with_adaptation()
-        )
-        print("")
-        print("mdsa_settings=")
-        pprint(mdsa_settings.__dict__)
-
-        # Generate test scope.
-        mdsa_settings.export_types = None
-        output_config: Output_config = create_default_output_config(
-            exp_config=mdsa_settings
-        )
-
-        # Generate run_configs
-        full_exp_runner = Experiment_runner(
-            exp_config=mdsa_settings,
-            output_config=output_config,
-            reverse=True,
-            perform_run=False,
-            specific_run_config=None,
-        )
-
-        for run_config in full_exp_runner.run_configs:
+        for (
+            run_config,
+            original_results_nx_graphs,
+        ) in run_config_results.items():
             print("run_config=")
             pprint(run_config.__dict__)
 
-            # Only test run_configs with adaptation for this test.
-            if list(run_config.adaptation.keys()) == ["redundancy"]:
-                # Get the original results dict to manually execute experiment.
-                original_results_nx_graphs: Dict = (
-                    full_exp_runner.perform_run_stage_1(
-                        exp_config=mdsa_settings,
-                        output_config=output_config,
-                        plot_config=get_default_plot_config(),
-                        run_config=run_config,
-                    )
-                )
-
-                # Generate lists with dead neurons that are to be considered
-                # dead during a run. One list contains all the neurons that
-                # will be dead due to radiation in a single run_config.
-                for dead_neuron_names in get_dead_neuron_name_cominations(
-                    original_results_nx_graphs["graphs_dict"]["snn_algo_graph"]
+            # Generate lists with dead neurons that are to be considered
+            # dead during a run. One list contains all the neurons that
+            # will be dead due to radiation in a single run_config.
+            for dead_neuron_names in get_dead_neuron_name_cominations(
+                snn_algo_graph=original_results_nx_graphs["graphs_dict"][
+                    "snn_algo_graph"
+                ]
+            ):
+                # Do not test redundancy for counter neuron, because they
+                # don't spike.
+                # Do not test redundancy for terminator node because it
+                # is ok if any of them fire simultaneously, so they are
+                # "dumb" copies, not intelligent redundancy.
+                if not any(
+                    x in dead_neuron_names[0]
+                    for x in ["counter", "terminator"]
                 ):
-                    # Do not test redundancy for counter neuron, because they
-                    # don't spike.
-                    # Do not test redundancy for terminator node because it
-                    # is ok if any of them fire simultaneously, so they are
-                    # "dumb" copies, not intelligent redundancy.
-                    if not any(
-                        x in dead_neuron_names[0]
-                        for x in ["counter", "terminator"]
+                    results_nx_graphs = overwrite_radiation_with_custom(
+                        original_results_nx_graphs=original_results_nx_graphs,
+                        dead_neuron_names=dead_neuron_names,
+                    )
+
+                    # Now that the graphs have been created, simulate them.
+                    sim_graphs(
+                        run_config=run_config,
+                        stage_1_graphs=results_nx_graphs["graphs_dict"],
+                    )
+
+                    # Perform actual test.
+                    assert_redundant_neuron_takes_over(
+                        dead_neuron_names=dead_neuron_names,
+                        graphs_dict=results_nx_graphs["graphs_dict"],
+                        output_config=output_config,
+                        max_redundancy=list(run_config.adaptation.values())[0],
+                        run_config=run_config,
+                        test_object=self,
+                    )
+
+                    # Then also verify the complete adapted algorithm
+                    # still works.
+                    if set_results(
+                        output_config=output_config,
+                        run_config=run_config,
+                        stage_2_graphs=results_nx_graphs["graphs_dict"],
                     ):
-                        # Create a deep copy from the results dict to prevent
-                        # the run_config from working with data changed by
-                        # the/a previous run_config.
-                        results_nx_graphs = copy.deepcopy(
-                            original_results_nx_graphs
+                        compute_results(
+                            results_nx_graphs=results_nx_graphs,
+                            stage_index=4,
                         )
-
-                        # Copy adapted graph into radiation graph to overwrite
-                        # radiation death. This is because the run_config uses
-                        # the seed to set radiation death, whereas this test
-                        # requires a specific set (dead_neuron_names) to die.
-                        results_nx_graphs["graphs_dict"][
-                            "rad_adapted_snn_graph"
-                        ] = copy.deepcopy(
-                            results_nx_graphs["graphs_dict"][
-                                "adapted_snn_graph"
-                            ]
-                        )
-                        rad_adapted_snn_graph = results_nx_graphs[
-                            "graphs_dict"
-                        ]["rad_adapted_snn_graph"]
-
-                        # Apply radiation based on the selected
-                        # dead_neuron_names for this run_config.
-                        for dead_neuron_name in dead_neuron_names:
-                            rad_adapted_snn_graph.nodes[dead_neuron_name][
-                                "rad_death"
-                            ] = True
-                            rad_adapted_snn_graph.nodes[dead_neuron_name][
-                                "nx_lif"
-                            ][0].vth.set(9999)
-                        verify_radiation_is_applied(
-                            some_graph=rad_adapted_snn_graph,
-                            dead_neuron_names=dead_neuron_names,
-                            rad_type="neuron_death",
-                        )
-
-                        # Now that the graphs have been created, simulate them.
-                        sim_graphs(
-                            run_config=run_config,
-                            stage_1_graphs=results_nx_graphs["graphs_dict"],
-                        )
-
-                        # Perform actual test.
-                        assert_redundant_neuron_takes_over(
-                            dead_neuron_names=dead_neuron_names,
-                            graphs_dict=results_nx_graphs["graphs_dict"],
-                            output_config=output_config,
-                            max_redundancy=list(
-                                run_config.adaptation.values()
-                            )[0],
-                            run_config=run_config,
-                            test_object=self,
-                        )
-
-                        # Then also verify the complete adapted algorithm
-                        # still works.
-                        if set_results(
-                            output_config=output_config,
-                            run_config=run_config,
-                            stage_2_graphs=results_nx_graphs["graphs_dict"],
-                        ):
-                            compute_results(
-                                results_nx_graphs=results_nx_graphs,
-                                stage_index=4,
-                            )
-
-
-@typechecked
-def get_dead_neuron_name_cominations(
-    snn_algo_graph: nx.DiGraph,
-) -> List[List[str]]:
-    """Returns dead neuron lists."""
-    combinations: List[List[str]] = []
-
-    for node_name in snn_algo_graph.nodes():
-        if "connector" not in node_name:
-            combinations.append([node_name])
-    return combinations
-
-
-@typechecked
-def assert_run_config_json_results(
-    *,
-    test_object: Any,
-    graphs_dict: Dict[str, Union[nx.Graph, nx.DiGraph]],
-    run_config: Run_config,
-) -> None:
-    """Verifies the results of a run config using the json result output.
-
-    TODO: update expected_stages.
-    """
-
-    # Verify results are as expected.
-    expected_node_names: Dict[str, int] = get_results(
-        input_graph=graphs_dict["input_graph"],
-        m_val=run_config.algorithm["MDSA"]["m_val"],
-        rand_props=graphs_dict["input_graph"].graph["alg_props"],
-        seed=run_config.seed,
-        size=run_config.graph_size,
-    )
-
-    for graph_name, snn_graph in graphs_dict.items():
-        if graph_name in ["snn_algo_graph", "adapted_snn_graph"]:
-            # Verify the SNN graphs have completed simulation stage 2.
-            test_object.assertTrue(
-                # pylint: disable=R1733
-                2
-                in graphs_dict[graph_name].graph["completed_stages"]
-            )
-
-            actual_node_names: Dict[str, int] = snn_graph.graph["results"]
-
-            # Remove the passed boolean, and redo results verification.
-            copy_actual_node_names = copy.deepcopy(actual_node_names)
-            copy_actual_node_names.pop("passed")
-
-            # Verify node names are identical.
-            test_object.assertEquals(
-                copy_actual_node_names.keys(), expected_node_names.keys()
-            )
-
-            # Verify the expected nodes are the same as the actual nodes.
-            for key, expected_val in expected_node_names.items():
-                test_object.assertEquals(
-                    expected_val, copy_actual_node_names[key]
-                )
-
-            test_object.assertTrue(actual_node_names["passed"])
